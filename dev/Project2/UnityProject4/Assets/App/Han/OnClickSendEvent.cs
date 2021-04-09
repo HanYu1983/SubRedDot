2f61e40a142760fdda2df45c41958adc9b1156fe
hanyu <hanyushome@gmail.com>
2021-04-09 22:00:28 +0800

    add test scene add unitRx

---
+++ dev/Project2/UnityProject4/Assets/App/Han/OnClickSendEvent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OnClickSendEvent : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public string eventName;
+    public void OnPointerDown(PointerEventData pointerEventData)
+    {
+        Events.onButtonDown.OnNext(eventName);
+    }
+
+    //Detect if clicks are no longer registering
+    public void OnPointerUp(PointerEventData pointerEventData)
+    {
+        Events.onButtonUp.OnNext(eventName);
+    }
+}
