67c09b270f1d48825c3ea71c26c3440e365529b6
finalsteve <finalsteve@gmail.com>
2024-05-11 14:40:57 +0800

    [Vic] update

---
+++ dev/Project2/UnityProject4/Assets/Scripts/TitleController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleController : MonoBehaviour
+{
+
+    public AudioController audioController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(audioController);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnBtnStartClick()
+    {
+        Debug.Log("OnBtnStartClick");
+
+        audioController.PlayAudioById(0);
+    }
+}
