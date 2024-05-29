515895c5cdf6d02ed9976e00105dec09c348b9c4
finalsteve <finalsteve@gmail.com>
2024-05-29 23:53:20 +0800

    [Vic] new framework

---
+++ dev/Project2/UnityProject4/Assets/App2/Scripts/Controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public SceneLoader loader;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        JumpToScene(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void JumpToScene(int id)
+    {
+        loader.StartLoad(id);
+    }
+}
---
+++ dev/Project2/UnityProject4/Assets/App2/Scripts/SceneLoader.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public Controller controller;
+    public RectTransform container;
+    public List<string> scenes = new List<string>();
+
+    private GameObject currentPage;
+    public GameObject loadingPage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartLoad(int id)
+    {
+        loadingPage.SetActive(true);
+        StartCoroutine(LoadAsyncScene(id));
+    }
+    public IEnumerator LoadAsyncScene(int id)
+    {
+        ResourceRequest request = Resources.LoadAsync<GameObject>(scenes[id]);
+
+        while (!request.isDone)
+        {
+            yield return null; 
+        }
+
+        if(currentPage != null) { Destroy(currentPage); }
+
+        GameObject page = Instantiate(request.asset, container) as GameObject;
+        page.GetComponent<APage>().controller = controller;
+
+        currentPage = page;
+
+        loadingPage.SetActive(false);
+    }
+}
