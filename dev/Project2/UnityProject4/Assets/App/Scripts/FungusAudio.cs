bbea932a414bae0ad953e2ddb719bb852f97c676
finalsteve <finalsteve@gmail.com>
2024-07-08 22:16:39 +0800

    新增音量控制方法

---
+++ dev/Project2/UnityProject4/Assets/App/Scripts/FungusAudio.cs
@@ -0,0 +1,21 @@
+﻿using Fungus;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FungusAudio : MonoBehaviour
+{
+    public Slider sliderAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sliderAudio.onValueChanged.AddListener(SetVolumn);
+    }
+
+    public void SetVolumn(float volume)
+    {
+        FungusManager.Instance.MusicManager.SetAudioVolume(volume, 0, null);
+    }
+}
