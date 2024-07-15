eee2ddbb7bf42d27bc7698df663d03ad05ab0713
lenalu <eve0904.gemma@msa.hinet.net>
2024-07-15 17:57:28 +0800

    標題畫面改

---
+++ dev/Project2/UnityProject4/Assets/App/Scripts/VolumeControl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeControl : MonoBehaviour
+{
+    public Slider musicSlider;
+    public Slider sfxSlider;
+    public AudioSource backgroundMusic;
+    public AudioSource sfxSource;
+
+    void Start()
+    {
+        // 設置初始值和添加監聽器
+        if (musicSlider != null)
+        {
+            musicSlider.value = backgroundMusic.volume;
+            musicSlider.onValueChanged.AddListener(SetMusicVolume);
+        }
+        if (sfxSlider != null)
+        {
+            sfxSlider.value = sfxSource.volume;
+            sfxSlider.onValueChanged.AddListener(SetSFXVolume);
+        }
+    }
+
+    void SetMusicVolume(float volume)
+    {
+        backgroundMusic.volume = volume;
+    }
+
+    void SetSFXVolume(float volume)
+    {
+        sfxSource.volume = volume;
+    }
+}
