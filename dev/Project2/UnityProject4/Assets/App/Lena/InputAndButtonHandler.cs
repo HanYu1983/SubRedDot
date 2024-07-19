15f6a883e8ebc14d4d0735712079e6253b8d0183
lenalu <eve0904.gemma@msa.hinet.net>
2024-07-19 20:10:05 +0800

    測試了取名~把動畫修改了一下

---
+++ dev/Project2/UnityProject4/Assets/App/Lena/InputAndButtonHandler.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class InputAndButtonHandler : MonoBehaviour
+{
+    public InputField inputField;
+    public Button submitButton;
+    private string inputValue;
+
+    void Start()
+    {
+        if (submitButton != null)
+        {
+            submitButton.onClick.AddListener(HandleButtonClick);
+        }
+        else
+        {
+            Debug.LogError("Submit Button not assigned");
+        }
+    }
+
+    void HandleButtonClick()
+    {
+        if (inputField != null)
+        {
+            inputValue = inputField.text;
+            Debug.Log("Input value when button clicked: " + inputValue);
+            // 這裡你可以添加更多代碼來處理輸入的值
+        }
+        else
+        {
+            Debug.LogError("InputField not assigned");
+        }
+    }
+}
