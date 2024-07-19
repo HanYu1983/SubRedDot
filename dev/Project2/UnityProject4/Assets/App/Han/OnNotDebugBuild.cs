9f79539ac07afe9860a94a67f6fa87af70a15a5b
hanyu <hanyushome@gmail.com>
2024-07-19 12:03:50 +0800

    no message

---
+++ dev/Project2/UnityProject4/Assets/App/Han/OnNotDebugBuild.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.App.Han
+{
+    public class OnNotDebugBuild : Base
+    {
+        void Awake()
+        {
+            if (Debug.isDebugBuild != true)
+            {
+                StartCoroutine(PerformAction());
+            }
+        }
+    }
+}
---
+++ dev/Project2/UnityProject4/Assets/App/Han/SetActiveAction.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.App.Han
+{
+    public class SetActiveAction : MonoBehaviour, IBaseButtonAction
+    {
+        public bool isActive;
+
+        public IEnumerator Perform()
+        {
+            gameObject.SetActive(isActive);
+            yield return null;
+        }
+    }
+}
